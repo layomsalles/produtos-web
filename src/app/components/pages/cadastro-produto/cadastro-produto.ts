3ca407d0793f51014325117f90732c82624c6c64
flipviduani <flipviduani@hotmail.com>
2026-07-28 13:05:53 -0300

    cadastro consulta e exclusao de produto e integração com o backend

--- src/app/components/pages/cadastro-produto/cadastro-produto.ts
+++ src/app/components/pages/cadastro-produto/cadastro-produto.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-cadastro-produto',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './cadastro-produto.html',
   styleUrl: './cadastro-produto.css',
 })
-export class CadastroProduto {}
+export class CadastroProduto {
+
+  //signal para armazenar a mensagem do servidor
+  mensagem = signal<string>('')
+
+  //HttpClient para integração com o backend
+  http = inject(HttpClient)
+
+  //criando formulario reativo
+  formCadastro = new FormGroup({
+    nome: new FormControl('', [
+      Validators.required, Validators.minLength(6), Validators.maxLength(150)
+    ]),
+
+    preco: new FormControl('', [
+      Validators.required, Validators.min(0.01), Validators.max(99999.99)
+    ]),
+
+    quantidade: new FormControl('', [
+      Validators.required, Validators.min(1), Validators.max(9999)
+    ])
+  })
+
+  //funcão para realizar o cadastro do produto
+  cadastrarProduto(){
+    //Requisição POST para a API cadastrar o produto
+    this.http.post(environment.apiUrl, this.formCadastro.value).subscribe((data: any) => {
+      //capturando a mensagem do backend para exibir na página
+      this.mensagem.set(data.menssagem)
+      //limpar o furmulario
+      this.formCadastro.reset()
+    })
+  }
+}
--- src/app/components/pages/consulta-produtos/consulta-produtos.ts
+++ src/app/components/pages/consulta-produtos/consulta-produtos.ts
@@ -1,21 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-consulta-produtos',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './consulta-produtos.html',
   styleUrl: './consulta-produtos.css',
 })
 export class ConsultaProdutos {
+
+  //Variavel para armazenar a listagem de produtos
+  produtos = signal<any[]>([])
+
   //Criar um objeto da biblioteca HttpClient
   http = inject(HttpClient)
 
   //Evento executando sempre que o component abre
   ngOnInit(){
      //Executando a conslta de produtos na API
-     this.http.get('http://localhost:5090/api/v1/Produtos').subscribe((produtos) => {
-      console.log(produtos)
+     this.http.get(environment.apiUrl).subscribe((data) => {
+      //Capturando o retorno da consulta
+      //Armazenadno o conteudo da variavel em um signial
+      this.produtos.set(data as any[])
      });
+  }
+
+  //Fução excutada qunado o botão de exclusão quando for clicado pelo usuario
+  excluirProduto(produto: any){
+    const confirmacao = confirm(`Deseja realmente excluir o produto "${produto.nome}"?`)
+
+    if(!confirmacao){ //Não confirmou a exclusão
+      return //Cancelar a função
+    }
+
+    //Executando a exclusão do produto na API
+    this.http.delete(`${environment.apiUrl}/${produto.id}`).subscribe((data: any) => {
+      alert(data.menssagem) // exibir uma pop-up com as mensagem
+      this.ngOnInit() //executando a consulta novamente
+    })
   }
 }
